3db65daa03d63fbe9b37e46e6877fd264459e082
Paulo Sousa <paulopgss@gmail.com>
2020-11-19 12:32:01 -0300

    estado e conexao com a api

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/logo.png'
 import api from '../../services/api'
 import { ContainerModal } from './style'
 
-const Modal = ({ id = 'modal', onClose = () => { } }) => {
+const Modal = ({ id = 'modal', onClose = () => { }, history }) => {
 
   const inputFile = useRef(null)
 
@@ -12,9 +12,9 @@ const Modal = ({ id = 'modal', onClose = () => { } }) => {
   const [file, setFile] = useState('')
 
   const submitNotice = () => {
-    
-    api.post('/news', {title, content, file}).then((e) => {
-      onClose(false)
+    api.post('/news', 
+      { title, content, file }).then((e) => {
+      history.goBack()
     })
   }
 
@@ -25,31 +25,42 @@ const Modal = ({ id = 'modal', onClose = () => { } }) => {
     <ContainerModal id={id} onClick={handleOutSide}>
       <div className="wrapper">
         <img src={Logo} alt="logo" />
-        <form action="">
           <span>Preencha os campos abaixo para adicionar uma notícia.</span>
           <form>
             <div className="input-img">
-              <input readOnly={true}
+              <input 
+                readOnly={true}
                 placeholder="Adicionar foto"
                 value={file}
                 style={{ cursor: 'pointer' }}
               />
-              <input type="file"
+              <input 
+                type="file"
                 ref={inputFile}
                 placeholder="Adicionar foto"
                 accept='image/png, image/jpeg'
                 style={{ display: 'none' }}
-                value={file}
-                onChange={e => setFile(inputFile.current.files[0].name)} />
-              <button onClick={() => inputFile.current.click()}>Pesquisar</button>
+                onChange={e => setFile(inputFile.current.files[0].name)} 
+                />
+              <button onClick={() => inputFile.current.click()} type="button">
+                Pesquisar
+              </button>
             </div>
             <div className="notice">
-              <input placeholder="Título" />
-              <textarea placeholder="Conteúdo" className="content-input" />
+              <input 
+                placeholder="Título"
+                value={title}
+                onChange={e => setTitle(e.target.value)} 
+              />
+              <textarea 
+                placeholder="Conteúdo" 
+                className="content-input"
+                value={content}
+                onChange={e => setContent(e.target.value)} 
+              />
               <button onClick={submitNotice}>Adicionar</button>
             </div>
           </form>
-        </form>
       </div>
     </ContainerModal>
   )
