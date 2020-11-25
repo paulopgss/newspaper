d29893a2b135a688e73746801fc09a1d7f8c74e3
Paulo Sousa <paulopgss@gmail.com>
2020-11-24 21:31:29 -0300

    Page profile criada

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import {List} from './pages/List'
 import Notice from './pages/Notice'
+import Profile from './pages/Profile'
 
 function Routes() {
     return(
         <BrowserRouter>
         <Route path="/" exact component={List} />
         <Route path="/news/:id" component={Notice} />
+        <Route path="/profile" component={Profile} />
         </BrowserRouter>
     )
 }
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Header from '../../components/Header'
+import { 
+  ContainerProfile,
+  Wrapper,
+  Span,
+  ImgProfile,
+  Inputs,
+  Input,
+  Button
+ } from './styles'
+
+ import ProfileImg from '../../assets/profile.jpg'
+
+function Profile() {
+  return(
+    <>
+    <Header />
+    <ContainerProfile>
+      <Wrapper>
+        <Span>Meus dados</Span>
+        <ImgProfile src={ProfileImg} />
+        <Inputs>
+        <Input placeholder="Nome" />
+        <Input placeholder="Email" />
+        <Span message>Altere os dados abaixo somente para realizar alteração de senha.</Span>
+        <Input placeholder="Senha atual" />
+        <Input placeholder="Nova senha" />
+        <Input placeholder="Confirme a nova senha" />
+        <Button>Salva</Button>
+        </Inputs>
+      </Wrapper>
+    </ContainerProfile>
+    </>
+  )
+}
+
+export default Profile
