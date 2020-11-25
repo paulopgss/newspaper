ac7840746604ad0e7cfa6dda08aadc5c05293689
Paulo Sousa <paulopgss@gmail.com>
2020-11-25 10:25:23 -0300

    Conexao do signup com a api

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import api from '../../services/api'
 import Logo from '../../assets/logo.png'
 import {
   ContainerModal,
@@ -10,12 +11,31 @@ import {
   Button
 } from './styles'
 
-import api from '../../services/api'
 
 function SignUp({ id = 'modal', onClose = () => { } }) {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [comppass, setComppass] = useState('')
+
+  const addUser = () => {
+    if (!name || !email || !password || !comppass) {
+      return alert('Todos os campos devem ser preenchidos')
+    } if (password != comppass) {
+      return alert('As senhas são diferentes')
+    }
+
+    api.post('/users', {name, email, password}).then(resp => {
+      if (resp.data.sucess) {
+        onClose(false)
+        return alert('Usuario cadastrado!')
+      }
+      alert(resp.data.message)
+
+    }).catch((err) => {
+      alert('Erro ao criar usuário!')
+    })
+  }
 
   const handleOutSide = (e) => {
     if (e.target.id === id) onClose()
@@ -27,23 +47,31 @@ function SignUp({ id = 'modal', onClose = () => { } }) {
         <SpanText>Criar uma conta</SpanText>
         <Form>
           <Input
-            title
+            value={name}
+            onChange={e => setName(e.target.value)}
             placeholder="Nome"
           />
           <Input
-            title
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            type="email"
             placeholder="Email"
           />
           <Input
-            title
+            value={password}
+            onChange={e => setPassword(e.target.value)}
             placeholder="Senha"
+            type="password"
           />
           <Input
-            title
+            value={comppass}
+            onChange={e => setComppass(e.target.value)}
             placeholder="Confirmar senha"
+            type="password"
           />
           <Button
             submit
+            onClick={addUser}
           >Criar</Button>
         </Form>
       </Wrapper>
